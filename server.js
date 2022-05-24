e19b1f7450104f28df8f18dc7495177480916adc
Marcus Westbrooks <marcus.westbrooks@gmail.com>
2022-05-23 23:30:14 -0400

    Hopefully last commit

--- server.js
+++ server.js
@@ -3,11 +3,7 @@
 const app = require("./app")
 const {PORT} = require("./config");
 
-//Connect to client
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static('teacher/build'))
-}
 
 
 app.listen(PORT, function() {
